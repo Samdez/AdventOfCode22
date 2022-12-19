7b81aad4c9f1e622ffa6b28718317f72dcc84eec
De Zaldua Samuel <samuel.dezaldua@gmail.com>
2022-12-19 21:05:57 +0100

    add 5.2

---
+++ 5/2.ts
@@ -0,0 +1,52 @@
+const input = Deno.readTextFileSync("./input.txt");
+const startHeight = 8;
+const numOfStacks = 9;
+
+const lines = input.split("\n");
+
+let crates: Record<string, string[]> = {};
+
+for (let i = 0; i < startHeight; i++) {
+	const line = lines[i];
+
+	let secIndex = 1;
+	for (let j = 0; j < numOfStacks * 4; j += 4) {
+		const crate = line[j] + line[j + 1] + line[j + 2];
+
+		if (crates[secIndex]) {
+			crates[secIndex].push(crate);
+		} else {
+			crates[secIndex] = [crate];
+		}
+		crates[secIndex] = crates[secIndex].filter(el => el !== "   ");
+		secIndex++;
+	}
+}
+
+for (let i = 0; i < numOfStacks; i++) {
+	const stacks = Object.values(crates);
+	stacks[i] = stacks[i].reverse();
+}
+
+for (let i = startHeight + 2; i < lines.length; i++) {
+	const line = lines[i].split(" ");
+
+	const numOfCratestoMove = +line[1];
+	const originStack = +line[3];
+	const destinationStack = +line[5];
+
+	const cratesToMove = crates[originStack].splice(-numOfCratestoMove)!;
+
+	crates[destinationStack].push(...cratesToMove);
+}
+
+let res = "";
+Object.values(crates).forEach(stack => {
+	res += stack.length ? stack[stack.length - 1] : " ";
+});
+res = res
+	.split("")
+	.filter(char => !char.match(/\[|\]/))
+	.join("");
+
+console.log(res);
